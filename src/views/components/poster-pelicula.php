9048e6665480ca92564f6f2e072ae1e4881caa48
yeicobF <A291423@alumnos.uaslp.mx>
2021-12-13 04:12:39 -0600

    finish poster mejores películas

--- src/views/components/poster-pelicula.php
+++ src/views/components/poster-pelicula.php
@@ -5,19 +5,39 @@ Podría haber 2 póster:
 - Con los detalles a la derecha.
 -->
 
-<!-- Póster de películas. -->
-<div class="movie-poster border border-secondary col-6 col-sm-4">
-  <figure class="">
-    <!-- 
-      Contenedor para que el año tenga posición relativa al póster y 
-      no a todo el contenedor. 
-    -->
-    <div class="movie-poster__year-image">
-      <a href="<?php echo DirectoryPath::VIEWS_URL; ?>peliculas/detalles-pelicula.php?id=id_pelicula" class="">
-        <img class="movie-poster__img" src="<?php echo DirectoryPath::IMG_URL; ?>movie-posters/spiderman-no-way-home/1.jpg" alt="Spiderman: No Way Home">
-      </a>
-      <time datetime="2021" class="movie-poster__year">2021</time>
-    </div>
-    <figcaption class="movie-poster__title">Spiderman: No Way Home</figcaption>
-  </figure>
-</div>
+<!-- Póster de películas con solo título. -->
+<figure class="movie-poster border border-secondary col-6 col-sm-4">
+  <!-- 
+    Contenedor para que el año tenga posición relativa al póster y 
+    no a todo el contenedor. 
+  -->
+  <div class="movie-poster__year-image">
+    <a href="<?php echo DirectoryPath::VIEWS_URL; ?>peliculas/detalles-pelicula.php?id=id_pelicula" class="">
+      <img class="movie-poster__img" src="<?php echo DirectoryPath::IMG_URL; ?>movie-posters/spiderman-no-way-home/1.jpg" alt="Spiderman: No Way Home">
+    </a>
+    <time datetime="2021" class="movie-poster__year">2021</time>
+  </div>
+  <figcaption class="movie-poster__title">Spiderman: No Way Home</figcaption>
+</figure>
+
+
+<!-- Póster de películas con todo y detalles. -->
+<figure class="row movie-poster border border-secondary col-12">
+  <!--
+    Contenedor para que el año tenga posición relativa al póster y
+    no a todo el contenedor.
+  -->
+  <div class="movie-poster__year-image col-6">
+    <a href="<?php echo DirectoryPath::VIEWS_URL; ?>peliculas/detalles-pelicula.php?id=id_pelicula" class="">
+      <img class="movie-poster__img" src="<?php echo DirectoryPath::IMG_URL; ?>movie-posters/spiderman-no-way-home/1.jpg" alt="Spiderman: No Way Home">
+    </a>
+    <time datetime="2021" class="movie-poster__year">2021</time>
+  </div>
+  <figcaption class="col-6">
+    <ul class="best-movies__details-list">
+      <li class="movie-poster__title">Spiderman: No Way Home</li>
+      <li>4.5/5</li>
+      <li><time datetime="PT2H28M">2h 38m</time></li>
+    </ul>
+  </figcaption>
+</figure>
--- src/views/index.php
+++ src/views/index.php
@@ -52,82 +52,149 @@ $baseHtmlHead = new BaseHtmlHead(
       <!-- Contenedor de películas. -->
       <div class="movies-container border border-primary col-12 col-sm-8">
         <h2 class="movies-container__title">Películas</h2>
-        <div class="row movies__row border border-secondary">
+        <div class="row border border-secondary">
           <!-- Póster de películas. -->
-          <div class="movie-poster border border-secondary col-6 col-sm-4">
-            <figure class="">
-              <!-- 
-                Contenedor para que el año tenga posición relativa al póster y 
-                no a todo el contenedor. 
-              -->
-              <div class="movie-poster__year-image">
-                <a href="<?php echo DirectoryPath::VIEWS_URL; ?>peliculas/detalles-pelicula.php?id=id_pelicula" class="">
-                  <img class="movie-poster__img" src="<?php echo DirectoryPath::IMG_URL; ?>movie-posters/spiderman-no-way-home/1.jpg" alt="Spiderman: No Way Home">
-                </a>
-                <time datetime="2021" class="movie-poster__year">2021</time>
-              </div>
-              <figcaption class="movie-poster__title">Spiderman: No Way Home</figcaption>
-            </figure>
-          </div>
+          <figure class="movie-poster border border-secondary col-6 col-sm-4">
+            <!-- 
+              Contenedor para que el año tenga posición relativa al póster y 
+              no a todo el contenedor. 
+            -->
+            <div class="movie-poster__year-image">
+              <a href="<?php echo DirectoryPath::VIEWS_URL; ?>peliculas/detalles-pelicula.php?id=id_pelicula" class="">
+                <img class="movie-poster__img" src="<?php echo DirectoryPath::IMG_URL; ?>movie-posters/spiderman-no-way-home/1.jpg" alt="Spiderman: No Way Home">
+              </a>
+              <time datetime="2021" class="movie-poster__year">2021</time>
+            </div>
+            <figcaption class="movie-poster__title">Spiderman: No Way Home</figcaption>
+          </figure>
 
           <!-- Póster de películas. -->
-          <div class="movie-poster border border-secondary col-6 col-sm-4">
-            <figure class="">
-              <div class="movie-poster__year-image">
-                <a href="<?php echo DirectoryPath::VIEWS_URL; ?>peliculas/detalles-pelicula.php?id=id_pelicula" class="">
-                  <img class="movie-poster__img" src="<?php echo DirectoryPath::IMG_URL; ?>movie-posters/friday-the-13th/1.jpg" alt="Friday the 13th">
-                </a>
-                <time datetime="2021" class="movie-poster__year">1980</time>
-              </div>
-              <figcaption class="movie-poster__title">Friday the 13th</figcaption>
-            </figure>
-          </div>
+          <figure class="movie-poster border border-secondary col-6 col-sm-4">
+            <div class="movie-poster__year-image">
+              <a href="<?php echo DirectoryPath::VIEWS_URL; ?>peliculas/detalles-pelicula.php?id=id_pelicula" class="">
+                <img class="movie-poster__img" src="<?php echo DirectoryPath::IMG_URL; ?>movie-posters/friday-the-13th/1.jpg" alt="Friday the 13th">
+              </a>
+              <time datetime="2021" class="movie-poster__year">1980</time>
+            </div>
+            <figcaption class="movie-poster__title">Friday the 13th</figcaption>
+          </figure>
 
           <!-- Póster de películas. -->
-          <div class="movie-poster border border-secondary col-6 col-sm-4">
-            <figure class="">
-              <div class="movie-poster__year-image">
-                <a href="<?php echo DirectoryPath::VIEWS_URL; ?>peliculas/detalles-pelicula.php?id=id_pelicula" class="">
-                  <img class="movie-poster__img" src="<?php echo DirectoryPath::IMG_URL; ?>movie-posters/avengers-endgame/1.jpg" alt="Avengers: Endgame">
-                </a>
-                <time datetime="2021" class="movie-poster__year">2019</time>
-              </div>
-              <figcaption class="movie-poster__title">Avengers: Endgame</figcaption>
-            </figure>
-          </div>
+          <figure class="movie-poster border border-secondary col-6 col-sm-4">
+            <div class="movie-poster__year-image">
+              <a href="<?php echo DirectoryPath::VIEWS_URL; ?>peliculas/detalles-pelicula.php?id=id_pelicula" class="">
+                <img class="movie-poster__img" src="<?php echo DirectoryPath::IMG_URL; ?>movie-posters/avengers-endgame/1.jpg" alt="Avengers: Endgame">
+              </a>
+              <time datetime="2021" class="movie-poster__year">2019</time>
+            </div>
+            <figcaption class="movie-poster__title">Avengers: Endgame</figcaption>
+          </figure>
 
           <!-- Póster de películas. -->
-          <div class="movie-poster border border-secondary col-6 col-sm-4">
-            <figure class="">
-              <div class="movie-poster__year-image">
-                <a href="<?php echo DirectoryPath::VIEWS_URL; ?>peliculas/detalles-pelicula.php?id=id_pelicula" class="">
-                  <img class="movie-poster__img" src="<?php echo DirectoryPath::IMG_URL; ?>movie-posters/spiderman-no-way-home/1.jpg" alt="Spiderman: No Way Home">
-                </a>
-                <time datetime="2021" class="movie-poster__year">2021</time>
-              </div>
-              <figcaption class="movie-poster__title">Spiderman: No Way Home</figcaption>
-            </figure>
-          </div>
+          <figure class="movie-poster border border-secondary col-6 col-sm-4">
+            <div class="movie-poster__year-image">
+              <a href="<?php echo DirectoryPath::VIEWS_URL; ?>peliculas/detalles-pelicula.php?id=id_pelicula" class="">
+                <img class="movie-poster__img" src="<?php echo DirectoryPath::IMG_URL; ?>movie-posters/spiderman-no-way-home/1.jpg" alt="Spiderman: No Way Home">
+              </a>
+              <time datetime="2021" class="movie-poster__year">2021</time>
+            </div>
+            <figcaption class="movie-poster__title">Spiderman: No Way Home</figcaption>
+          </figure>
 
           <!-- Póster de películas. -->
-          <div class="movie-poster border border-secondary col-6 col-sm-4">
-            <figure class="">
-              <div class="movie-poster__year-image">
-                <a href="<?php echo DirectoryPath::VIEWS_URL; ?>peliculas/detalles-pelicula.php?id=id_pelicula" class="">
-                  <img class="movie-poster__img" src="<?php echo DirectoryPath::IMG_URL; ?>movie-posters/spiderman-no-way-home/1.jpg" alt="Spiderman: No Way Home">
-                </a>
-                <time datetime="2021" class="movie-poster__year">2021</time>
-              </div>
-              <figcaption class="movie-poster__title">Spiderman: No Way Home</figcaption>
-            </figure>
-          </div>
-
+          <figure class="movie-poster border border-secondary col-6 col-sm-4">
+            <div class="movie-poster__year-image">
+              <a href="<?php echo DirectoryPath::VIEWS_URL; ?>peliculas/detalles-pelicula.php?id=id_pelicula" class="">
+                <img class="movie-poster__img" src="<?php echo DirectoryPath::IMG_URL; ?>movie-posters/spiderman-no-way-home/1.jpg" alt="Spiderman: No Way Home">
+              </a>
+              <time datetime="2021" class="movie-poster__year">2021</time>
+            </div>
+            <figcaption class="movie-poster__title">Spiderman: No Way Home</figcaption>
+          </figure>
 
         </div>
       </div>
       <!-- Mejores películas. Es un sidebar. -->
       <div class="best-movies-container border border-primary col-12 col-sm-4">
         <h2 class="best-movies-container__title">Mejores Películas</h2>
+        <!-- Póster de películas. -->
+        <figure class="row movie-poster border border-secondary col-12">
+          <!--
+            Contenedor para que el año tenga posición relativa al póster y
+            no a todo el contenedor.
+          -->
+          <div class="movie-poster__year-image col-6">
+            <a href="<?php echo DirectoryPath::VIEWS_URL; ?>peliculas/detalles-pelicula.php?id=id_pelicula" class="">
+              <img class="movie-poster__img" src="<?php echo DirectoryPath::IMG_URL; ?>movie-posters/spiderman-no-way-home/1.jpg" alt="Spiderman: No Way Home">
+            </a>
+            <time datetime="2021" class="movie-poster__year">2021</time>
+          </div>
+          <figcaption class="col-6">
+            <ul class="best-movies__details-list">
+              <li class="movie-poster__title">Spiderman: No Way Home</li>
+              <li>4.5/5</li>
+              <li><time datetime="PT2H28M">2h 38m</time></li>
+            </ul>
+          </figcaption>
+        </figure>
+        <figure class="row movie-poster border border-secondary col-12">
+          <!--
+            Contenedor para que el año tenga posición relativa al póster y
+            no a todo el contenedor.
+          -->
+          <div class="movie-poster__year-image col-6">
+            <a href="<?php echo DirectoryPath::VIEWS_URL; ?>peliculas/detalles-pelicula.php?id=id_pelicula" class="">
+              <img class="movie-poster__img" src="<?php echo DirectoryPath::IMG_URL; ?>movie-posters/friday-the-13th/1.jpg" alt="Friday the 13th">
+            </a>
+            <time datetime="2021" class="movie-poster__year">1980</time>
+          </div>
+          <figcaption class="col-6">
+            <ul class="best-movies__details-list">
+              <li class="movie-poster__title">Friday the 13th</li>
+              <li>4/5</li>
+              <li><time datetime="PT2H35M">1h 35m</time></li>
+            </ul>
+          </figcaption>
+        </figure>
+        <!-- Póster de películas. -->
+        <figure class="row movie-poster border border-secondary col-12">
+          <!--
+            Contenedor para que el año tenga posición relativa al póster y
+            no a todo el contenedor.
+          -->
+          <div class="movie-poster__year-image col-6">
+            <a href="<?php echo DirectoryPath::VIEWS_URL; ?>peliculas/detalles-pelicula.php?id=id_pelicula" class="">
+              <img class="movie-poster__img" src="<?php echo DirectoryPath::IMG_URL; ?>movie-posters/spiderman-no-way-home/1.jpg" alt="Spiderman: No Way Home">
+            </a>
+            <time datetime="2021" class="movie-poster__year">2021</time>
+          </div>
+          <figcaption class="col-6">
+            <ul class="best-movies__details-list">
+              <li class="movie-poster__title">Spiderman: No Way Home</li>
+              <li>4.5/5</li>
+              <li><time datetime="PT2H28M">2h 38m</time></li>
+            </ul>
+          </figcaption>
+        </figure>
+        <figure class="row movie-poster border border-secondary col-12">
+          <!--
+            Contenedor para que el año tenga posición relativa al póster y
+            no a todo el contenedor.
+          -->
+          <div class="movie-poster__year-image col-6">
+            <a href="<?php echo DirectoryPath::VIEWS_URL; ?>peliculas/detalles-pelicula.php?id=id_pelicula" class="">
+              <img class="movie-poster__img" src="<?php echo DirectoryPath::IMG_URL; ?>movie-posters/friday-the-13th/1.jpg" alt="Friday the 13th">
+            </a>
+            <time datetime="2021" class="movie-poster__year">1980</time>
+          </div>
+          <figcaption class="col-6">
+            <ul class="best-movies__details-list">
+              <li class="movie-poster__title">Friday the 13th</li>
+              <li>4/5</li>
+              <li><time datetime="PT2H35M">1h 35m</time></li>
+            </ul>
+          </figcaption>
+        </figure>
       </div>
     </div>
   </div>
