75eceb941fc309e5903b0150d35038137a7d6ccb
yeicobF <A291423@alumnos.uaslp.mx>
2021-12-14 05:51:15 -0600

    form nuevo comentario terminado

---
+++ src/views/components/poster-pelicula-detalles-derecha.php
@@ -0,0 +1,20 @@
+<!-- Póster de películas con todo y detalles a la derecha. -->
+<figure class="row movie-poster   col-12">
+  <!--
+    Contenedor para que el año tenga posición relativa al póster y
+    no a todo el contenedor.
+  -->
+  <div class="movie-poster__year-image col-6">
+    <a rel="noopener noreferrer" href="<?php echo $views_folder; ?>peliculas/detalles-pelicula/index.php?id=id_pelicula" class="">
+      <img class="movie-poster__img" src="<?php echo DirectoryPath::getPathWithLocalhost(DirectoryPath::ASSETS); ?>img/movie-posters/spiderman-no-way-home/1.jpg" alt="Spiderman: No Way Home">
+    </a>
+    <time datetime="2021" class="movie-poster__year">2021</time>
+  </div>
+  <figcaption class="col-6">
+    <ul class="best-movies__details-list">
+      <li class="movie-poster__title">Spiderman: No Way Home</li>
+      <li><data value="4.5">4.5/5</data></li>
+      <li><time datetime="PT2H28M">2h 38m</time></li>
+    </ul>
+  </figcaption>
+</figure>
--- src/views/peliculas/detalles-pelicula/index.php
+++ src/views/peliculas/detalles-pelicula/index.php
@@ -40,6 +40,7 @@ $baseHtmlHead = new BaseHtmlHead(
   <link rel="stylesheet" href="<?php echo $src_folder; ?>css/transformations/rotate.css">
   <link rel="stylesheet" href="<?php echo $src_folder; ?>css/movies/movies.css">
   <link rel="stylesheet" href="<?php echo $src_folder; ?>css/movies/movie-details.css">
+  <link rel="stylesheet" href="<?php echo $src_folder; ?>css/movies/comments.css">
   <link rel="stylesheet" href="<?php echo $src_folder; ?>css/footer/footer.css">
 
   <!-- SCRIPTS -->
@@ -112,6 +113,99 @@ $baseHtmlHead = new BaseHtmlHead(
         </ul>
       </section>
     </main>
+    <div class="row">
+      <section class="comments__container col-12 col-sm-8">
+        <h2>Comentarios</h2>
+        <form action="" method="post" class="comments__form">
+          <!-- Hay que definir el método a utilizar con un input hidden. -->
+          <input type="hidden" name="_method" value="POST">
+          
+          <textarea placeholder="Ingresa un comentario" name="nuevo-comentario" id="" rows="5"></textarea>
+          <input type="submit" value="Publicar" class="btn btn-info">
+        </form>
+      </section>
+      <aside class="best-movies-container col-12 col-sm-4">
+        <h2 class="best-movies-container__title">Mejores Películas</h2>
+        <!-- Póster de películas. -->
+        <figure class="row movie-poster   col-12">
+          <!--
+            Contenedor para que el año tenga posición relativa al póster y
+            no a todo el contenedor.
+          -->
+          <div class="movie-poster__year-image col-6">
+            <a rel="noopener noreferrer" href="<?php echo $views_folder; ?>peliculas/detalles-pelicula/index.php?id=id_pelicula" class="">
+              <img class="movie-poster__img" src="<?php echo DirectoryPath::getPathWithLocalhost(DirectoryPath::ASSETS); ?>img/movie-posters/spiderman-no-way-home/1.jpg" alt="Spiderman: No Way Home">
+            </a>
+            <time datetime="2021" class="movie-poster__year">2021</time>
+          </div>
+          <figcaption class="col-6">
+            <ul class="best-movies__details-list">
+              <li class="movie-poster__title">Spiderman: No Way Home</li>
+              <li><data value="4.5">4.5/5</data></li>
+              <li><time datetime="PT2H28M">2h 38m</time></li>
+            </ul>
+          </figcaption>
+        </figure>
+        <figure class="row movie-poster   col-12">
+          <!--
+            Contenedor para que el año tenga posición relativa al póster y
+            no a todo el contenedor.
+          -->
+          <div class="movie-poster__year-image col-6">
+            <a rel="noopener noreferrer" href="<?php echo $views_folder; ?>peliculas/detalles-pelicula/index.php?id=id_pelicula" class="">
+              <img class="movie-poster__img" src="<?php echo DirectoryPath::getPathWithLocalhost(DirectoryPath::ASSETS); ?>img/movie-posters/friday-the-13th/1.jpg" alt="Friday the 13th">
+            </a>
+            <time datetime="2021" class="movie-poster__year">1980</time>
+          </div>
+          <figcaption class="col-6">
+            <ul class="best-movies__details-list">
+              <li class="movie-poster__title">Friday the 13th</li>
+              <li><data value="4">4/5</data></li>
+              <li><time datetime="PT2H35M">1h 35m</time></li>
+            </ul>
+          </figcaption>
+        </figure>
+        <!-- Póster de películas. -->
+        <figure class="row movie-poster   col-12">
+          <!--
+            Contenedor para que el año tenga posición relativa al póster y
+            no a todo el contenedor.
+          -->
+          <div class="movie-poster__year-image col-6">
+            <a rel="noopener noreferrer" href="<?php echo $views_folder; ?>peliculas/detalles-pelicula/index.php?id=id_pelicula" class="">
+              <img class="movie-poster__img" src="<?php echo DirectoryPath::getPathWithLocalhost(DirectoryPath::ASSETS); ?>img/movie-posters/spiderman-no-way-home/1.jpg" alt="Spiderman: No Way Home">
+            </a>
+            <time datetime="2021" class="movie-poster__year">2021</time>
+          </div>
+          <figcaption class="col-6">
+            <ul class="best-movies__details-list">
+              <li class="movie-poster__title">Spiderman: No Way Home</li>
+              <li><data value="4.5">4.5/5</data></li>
+              <li><time datetime="PT2H28M">2h 38m</time></li>
+            </ul>
+          </figcaption>
+        </figure>
+        <figure class="row movie-poster   col-12">
+          <!--
+            Contenedor para que el año tenga posición relativa al póster y
+            no a todo el contenedor.
+          -->
+          <div class="movie-poster__year-image col-6">
+            <a rel="noopener noreferrer" href="<?php echo $views_folder; ?>peliculas/detalles-pelicula/index.php?id=id_pelicula" class="">
+              <img class="movie-poster__img" src="<?php echo DirectoryPath::getPathWithLocalhost(DirectoryPath::ASSETS); ?>img/movie-posters/friday-the-13th/1.jpg" alt="Friday the 13th">
+            </a>
+            <time datetime="2021" class="movie-poster__year">1980</time>
+          </div>
+          <figcaption class="col-6">
+            <ul class="best-movies__details-list">
+              <li class="movie-poster__title">Friday the 13th</li>
+              <li><data value="4">4/5</data></li>
+              <li><time datetime="PT2H35M">1h 35m</time></li>
+            </ul>
+          </figcaption>
+        </figure>
+      </aside>
+    </div>
   </div>
 
 
