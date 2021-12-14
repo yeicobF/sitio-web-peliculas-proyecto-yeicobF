f5f38623cecc22bfe78acf1184218cf70875dad0
yeicobF <A291423@alumnos.uaslp.mx>
2021-12-14 07:26:07 -0600

    finish: comentarios película + detalles película
    Ya está terminada esa parte en cuanto al frontend. Faltaría implementar
    la funcionalidad.

--- src/views/peliculas/detalles-pelicula/index.php
+++ src/views/peliculas/detalles-pelicula/index.php
@@ -119,10 +119,65 @@ $baseHtmlHead = new BaseHtmlHead(
         <form action="" method="post" class="comments__form">
           <!-- Hay que definir el método a utilizar con un input hidden. -->
           <input type="hidden" name="_method" value="POST">
-          
+
           <textarea placeholder="Ingresa un comentario" name="nuevo-comentario" id="" rows="5"></textarea>
           <input type="submit" value="Publicar" class="btn btn-info">
         </form>
+
+        <!-- Comentarios ya publicados. -->
+        <article class="comments__posted">
+          <figure class="comments__details">
+            <img src="<?php echo $img_folder; ?>../avatar/1.jpg" alt="Username" class="comments__details__logo">
+            <figcaption class="comments__details__info">
+              <h3 class="comments__details__title">Username Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic ad numquam natus quaerat maiores vitae voluptatem. Voluptates perspiciatis sequi delectus, illum adipisci earum modi, error, ad totam eos praesentium hic?</h3>
+              <time class="comments__details__time-ago" datetime="PT3H">Hace 3 horas</time>
+            </figcaption>
+          </figure>
+          <main class="comments__text">
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellendus accusantium officiis deserunt cum temporibus iure in sed alias a corporis?
+            </p>
+          </main>
+          <footer class="comments__interaction">
+            <form name="comment-likes" class="" action="" method="post">
+              <button class="comments__interaction__button" type="button"><i class="fas fa-thumbs-up"></i></button>
+              <data value="2">2</data>
+            </form>
+            <form name="comment-dislikes" class="" action="" method="post">
+              <button class="comments__interaction__button selected" type="button"><i class="fas fa-thumbs-down"></i></button>
+              <data value="4">4</data>
+            </form>
+          </footer>
+        </article>
+        <!-- Comentarios ya publicados. -->
+        <article class="comments__posted">
+          <figure class="comments__details">
+            <img src="<?php echo $img_folder; ?>../avatar/1.jpg" alt="Username" class="comments__details__logo">
+            <figcaption class="comments__details__info">
+              <h3 class="comments__details__title">Username Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic ad numquam natus quaerat maiores vitae voluptatem. Voluptates perspiciatis sequi delectus, illum adipisci earum modi, error, ad totam eos praesentium hic?</h3>
+              <time class="comments__details__time-ago" datetime="PT3H">Hace 3 horas</time>
+            </figcaption>
+          </figure>
+          <main class="comments__text">
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellendus accusantium officiis deserunt cum temporibus iure in sed alias a corporis?
+            </p>
+          </main>
+          <footer class="comments__interaction">
+            <form name="comment-likes" class="" action="" method="post">
+              <button class="comments__interaction__button" type="button"><i class="fas fa-thumbs-up"></i></button>
+              <data value="2">2</data>
+            </form>
+            <form name="comment-dislikes" class="" action="" method="post">
+              <button class="comments__interaction__button selected" type="button"><i class="fas fa-thumbs-down"></i></button>
+              <data value="4">4</data>
+            </form>
+          </footer>
+        </article>
+
+        <?php 
+        include $path . DirectoryPath::VIEWS . "components/posted-comment.php"; 
+        ?>
       </section>
       <aside class="best-movies-container col-12 col-sm-4">
         <h2 class="best-movies-container__title">Mejores Películas</h2>
