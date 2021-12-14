5207910082cdc9b90cfe423d723c95581285edc3
yeicobF <A291423@alumnos.uaslp.mx>
2021-12-14 04:01:39 -0600

    movie-details__info background

--- src/views/peliculas/detalles-pelicula/index.php
+++ src/views/peliculas/detalles-pelicula/index.php
@@ -67,16 +67,17 @@ $baseHtmlHead = new BaseHtmlHead(
             <h2>Nombre original</h2>
           </header>
           <section class="movie-details__info">
+            <time datetime="2021" class="movie-details__year">2021</time>
             <!-- Calificaciones de la película. -->
             <data value="4.5" class="movie-details__user-rating">4.5/5</data>
             <time datetime="PT2H28M">2h 38m</time>
-            <data value="4.5" class="movie-details__age-rating">
+            <data value="18" class="movie-details__age-rating">
               Clasificación: 18+
             </data>
           </section>
           <p class="movie-details__synopsis">
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sint iste, ratione consequatur aliquid dolores cupiditate facere molestiae alias officia nisi totam modi ullam. Praesentium adipisci expedita iusto ullam deserunt illo?
-            
+
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sint iste, ratione consequatur aliquid dolores cupiditate facere molestiae alias officia nisi totam modi ullam. Praesentium adipisci expedita iusto ullam deserunt illo?
           </p>
           <ul class="movie-details__cast">
