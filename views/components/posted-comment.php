cd90ec6d40522d80b3404bbfb6f7f354669e9572
yeicobF <yeicobF@users.noreply.github.com>
2021-12-14 13:28:31 +0000

    deploy: f1cff9d0b89dcda6197faf8073743005a4c7cd6e

---
+++ views/components/posted-comment.php
@@ -0,0 +1,26 @@
+<!-- Comentarios ya publicados. -->
+<article class="comments__posted">
+  <figure class="comments__details">
+    <img src="<?php echo $img_folder; ?>../avatar/1.jpg" alt="Username" class="comments__details__logo">
+    <figcaption class="comments__details__info">
+      <h3 class="comments__details__title">Username Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic ad numquam natus quaerat maiores vitae voluptatem. Voluptates perspiciatis sequi delectus, illum adipisci earum modi, error, ad totam eos praesentium hic?</h3>
+      <time class="comments__details__time-ago" datetime="PT3H">Hace 3 horas</time>
+    </figcaption>
+  </figure>
+  <main class="comments__text">
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellendus accusantium officiis deserunt cum temporibus iure in sed alias a corporis?
+    </p>
+  </main>
+  <footer class="comments__interaction">
+    <form name="comment-likes" class="" action="" method="post">
+      <button class="comments__interaction__button" type="button"><i class="fas fa-thumbs-up"></i></button>
+      <data value="2">2</data>
+    </form>
+    <form name="comment-dislikes" class="" action="" method="post">
+      <button class="comments__interaction__button selected" type="button"><i class="fas fa-thumbs-down"></i></button>
+      <data value="4">4</data>
+    </form>
+  </footer>
+</article>
+</section>
