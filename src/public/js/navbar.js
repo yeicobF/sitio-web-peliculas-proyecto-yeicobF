4653f289eae34e0afd884ca3a8fbce21c3a02b41
yeicobF <A291423@alumnos.uaslp.mx>
2021-12-23 15:06:58 -0600

    refactor: rutas + movies layouts + html css

--- src/public/js/navbar.js
+++ src/public/js/navbar.js
@@ -1,6 +1,7 @@
-import {
-  activateToggleRotate180
-} from "../js/toggle/toggle-rotate.js";
+import { activateToggleRotate180 } from "../js/toggle/toggle-rotate.js";
 
 // Activamos la rotación de los elementos del navbar con la clase rotate.
-activateToggleRotate180(".navbar-nav .nav-link", ".rotate");
+activateToggleRotate180(
+  ".navbar-nav > .nav-item.dropdown > .nav-link.dropdown-toggle",
+  ".rotate",
+);
