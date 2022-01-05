c4a9a69412e9397bb39074c29141f655e81b391b
yeicobF <A291423@alumnos.uaslp.mx>
2022-01-05 08:21:36 -0600

    mejores-peliculas: cambio estilos y acomodo

--- src/views/layouts/movies/mejores-peliculas.php
+++ src/views/layouts/movies/mejores-peliculas.php
@@ -2,18 +2,18 @@
 <aside class="best-movies-container col-12 col-md-4">
   <h2 class="best-movies-container__title">Mejores Películas</h2>
   <!-- Póster de películas. -->
-  <figure class="row movie-poster   col-12">
+  <figure class="row movie-poster">
     <!--
           Contenedor para que el año tenga posición relativa al póster y
           no a todo el contenedor.
         -->
-    <div class="movie-poster__year-image col-6">
+    <div class="movie-poster__year-image col-6 col-md-5 col-lg-6">
       <a rel="noopener noreferrer" href="<?php echo VIEWS_FOLDER; ?>peliculas/detalles-pelicula/index.php?id=id_pelicula" class="">
         <img class="movie-poster__img" src="<?php echo ASSETS_FOLDER; ?>img/movie-posters/spiderman-no-way-home/1.jpg" alt="Spiderman: No Way Home">
       </a>
       <time datetime="2021" class="movie-poster__year">2021</time>
     </div>
-    <figcaption class="col-6">
+    <figcaption class="col-6 col-md-7 col-lg-6">
       <ul class="best-movies__details-list">
         <li class="movie-poster__title">Spiderman: No Way Home</li>
         <li><data value="4.5">4.5/5</data></li>
@@ -21,18 +21,18 @@
       </ul>
     </figcaption>
   </figure>
-  <figure class="row movie-poster   col-12">
+  <figure class="row movie-poster">
     <!--
           Contenedor para que el año tenga posición relativa al póster y
           no a todo el contenedor.
         -->
-    <div class="movie-poster__year-image col-6">
+    <div class="movie-poster__year-image col-6  col-md-5 col-lg-6">
       <a rel="noopener noreferrer" href="<?php echo VIEWS_FOLDER; ?>peliculas/detalles-pelicula/index.php?id=id_pelicula" class="">
         <img class="movie-poster__img" src="<?php echo ASSETS_FOLDER; ?>img/movie-posters/friday-the-13th/1.jpg" alt="Friday the 13th">
       </a>
       <time datetime="2021" class="movie-poster__year">1980</time>
     </div>
-    <figcaption class="col-6">
+    <figcaption class="col-6 col-md-7 col-lg-6">
       <ul class="best-movies__details-list">
         <li class="movie-poster__title">Friday the 13th</li>
         <li><data value="4">4/5</data></li>
@@ -40,19 +40,18 @@
       </ul>
     </figcaption>
   </figure>
-  <!-- Póster de películas. -->
-  <figure class="row movie-poster   col-12">
+  <figure class="row movie-poster">
     <!--
           Contenedor para que el año tenga posición relativa al póster y
           no a todo el contenedor.
         -->
-    <div class="movie-poster__year-image col-6">
+    <div class="movie-poster__year-image col-6  col-md-5 col-lg-6">
       <a rel="noopener noreferrer" href="<?php echo VIEWS_FOLDER; ?>peliculas/detalles-pelicula/index.php?id=id_pelicula" class="">
         <img class="movie-poster__img" src="<?php echo ASSETS_FOLDER; ?>img/movie-posters/spiderman-no-way-home/1.jpg" alt="Spiderman: No Way Home">
       </a>
       <time datetime="2021" class="movie-poster__year">2021</time>
     </div>
-    <figcaption class="col-6">
+    <figcaption class="col-6 col-md-7 col-lg-6">
       <ul class="best-movies__details-list">
         <li class="movie-poster__title">Spiderman: No Way Home</li>
         <li><data value="4.5">4.5/5</data></li>
@@ -60,18 +59,18 @@
       </ul>
     </figcaption>
   </figure>
-  <figure class="row movie-poster   col-12">
+  <figure class="row movie-poster">
     <!--
           Contenedor para que el año tenga posición relativa al póster y
           no a todo el contenedor.
         -->
-    <div class="movie-poster__year-image col-6">
+    <div class="movie-poster__year-image col-6  col-md-5 col-lg-6">
       <a rel="noopener noreferrer" href="<?php echo VIEWS_FOLDER; ?>peliculas/detalles-pelicula/index.php?id=id_pelicula" class="">
         <img class="movie-poster__img" src="<?php echo ASSETS_FOLDER; ?>img/movie-posters/friday-the-13th/1.jpg" alt="Friday the 13th">
       </a>
       <time datetime="2021" class="movie-poster__year">1980</time>
     </div>
-    <figcaption class="col-6">
+    <figcaption class="col-6 col-md-7 col-lg-6">
       <ul class="best-movies__details-list">
         <li class="movie-poster__title">Friday the 13th</li>
         <li><data value="4">4/5</data></li>
@@ -79,4 +78,5 @@
       </ul>
     </figcaption>
   </figure>
+
 </aside>
