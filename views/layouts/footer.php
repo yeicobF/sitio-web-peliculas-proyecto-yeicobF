bb1f17e013428fe7153e8666463cb3bbe707f5e5
yeicobF <yeicobF@users.noreply.github.com>
2022-01-07 08:23:34 +0000

    deploy: ee462abd43b59257619272df75ffaa5c0679dc1c

--- views/layouts/footer.php
+++ views/layouts/footer.php
@@ -1,7 +1,11 @@
+<?php
+$current_year = date("Y");
+?>
+
 <footer class="footer">
   <ul class="footer__elements container-md">
     <li>Francisco Jacob Flores Rodríguez</li>
-    <li>© 2021 Colección de películas</li>
+    <li>© 2021 - <?php echo $current_year; ?> Colección de películas</li>
     <li>Fundamentos de Desarrollo Web</li>
   </ul>
 </footer>
